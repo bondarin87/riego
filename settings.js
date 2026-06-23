50011d55bac2f51bc2d5ba9fa3ee45b1108eb4c7
bondarin87 <110008441+bondarin87@users.noreply.github.com>
2026-06-23 10:47:30 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,9 @@
 module.exports = {
+    uiPort: process.env.PORT || 8080,
+    uiHost: "0.0.0.0",
     flowFile: 'flows.json',
     userDir: __dirname,
     functionGlobalContext: {},
-    webDir: __dirname + '/public',
-    dashboard: {
-        title: 'Riego Garden Panel'
-    },
     logging: {
         console: {
             level: "info",
