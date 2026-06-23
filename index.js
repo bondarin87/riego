52e006200338874e2982acc1c4bf7f617a6e2842
bondarin87 <110008441+bondarin87@users.noreply.github.com>
2026-06-23 10:31:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const { exec } = require('child_process');
+const http = require('http');
+const express = require('express');
+const RED = require('node-red');
 
-// Arranca Node-RED usando los comandos del sistema
-const nodeRed = exec('npm start');
+const app = express();
+app.use("/", express.static("public"));
 
-nodeRed.stdout.on('data', (data) => {
-    console.log(data.toString());
-});
+const server = http.createServer(app);
 
-nodeRed.stderr.on('data', (data) => {
-    console.error(data.toString());
-});
+// Configuración de arranque para Node-RED
+const settings = require('./settings.js');
 
-nodeRed.on('exit', (code) => {
-    console.log(`Node-RED se detuvo con el código: ${code}`);
-    process.exit(code);
+RED.init(server, settings);
+app.use(settings.httpAdminRoot || "/admin", RED.httpAdmin);
+app.use(settings.httpNodeRoot || "/", RED.httpNode);
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+    console.log(`[OK] Servidor escuchando en el puerto ${port}`);
+    RED.start();
 });
